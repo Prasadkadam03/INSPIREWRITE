c2b5d18b205969146237d270ff89ba60c7caf876
Prasadkadam03 <prasadkadam29503@gmail.com>
2025-02-06 19:46:35 +0530

    complete boilerplate setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL :string
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL, 
+  }).$extends(withAccelerate());
+  
+  return c.text('Hello Hono!'); 
 })
 
-app.post('/api/v1/signup',(c) => {
-  return c.text("signup");
-} )
-
-app.post('/api/v1/signin',(c) => {
+app.post('/api/v1/signin', (c) => {
   return c.text("signin");
-} )
+})
 
-app.post('/api/v1/blog',(c) => {
+app.post('/api/v1/blog', (c) => {
   return c.text("post blog");
-} )
+})
 
-app.put('/api/v1/blog',(c) => {
+app.put('/api/v1/blog', (c) => {
   return c.text("put blog");
-} )
+})
 
-app.get('/api/v1/blog/:id',(c) => {
+app.get('/api/v1/blog/:id', (c) => {
   return c.text("blog/:id");
-} )
+})
 
 export default app
