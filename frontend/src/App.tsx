a2cb3c1811eb96c1cfe14d6c6d3b86507b1fbb00
Prasadkadam03 <prasadkadam29503@gmail.com>
2025-02-08 22:02:15 +0530

    signup signin Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css'
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Blog } from './pages/Blog';
 
 function App() {
-
   return (
-    <>
-      heyyy
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
