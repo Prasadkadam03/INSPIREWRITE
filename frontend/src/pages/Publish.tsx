339df01dda349912b084c340404e65ae6a480468
Prasadkadam03 <prasadkadam29503@gmail.com>
2025-02-22 18:51:40 +0530

    fix minor bug

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -6,24 +6,31 @@ import { ChangeEvent, useState } from "react";
 
 export const Publish = () => {
     const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+    const [content, setContent] = useState("");
+    const [area , setArea] = useState("");
     const navigate = useNavigate();
 
     return <div>
         <Appbar />
         <div className="flex justify-center w-full pt-8"> 
-            <div className="max-w-screen-lg w-full">
+            <div className="max-w-screen-lg  w-full">
                 <input onChange={(e) => {
                     setTitle(e.target.value)
                 }} type="text" className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Title" />
-
+                
+                <div className="pt-2">
+                <input onChange={(e) => {
+                    setArea(e.target.value)
+                }} type="text" className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Area of Blog" />
+                </div>
                 <TextEditor onChange={(e) => {
-                    setDescription(e.target.value)
+                    setContent(e.target.value)
                 }} />
                 <button onClick={async () => {
                     const response = await axios.post(`${BACKEND_URL}/api/v1/blog`, {
                         title,
-                        content: description
+                        content,
+                        area
                     }, {
                         headers: {
                             Authorization: localStorage.getItem("token")
